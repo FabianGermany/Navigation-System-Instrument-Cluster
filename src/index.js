a3255d6f35e992352eb0e9045845a8df42992a32
Fabian Luettel <fabianluettel@web.de>
2021-05-12 14:32:45 +0200

    change name import rotate

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@
 import * as app from './js/app';
 import { api } from 'agl-js-api';
 
-import * as leaflet from './leaflet/leaflet'; //also need the original version due to compass etc.
-import './leaflet-rotate/leaflet-src';
+// import * as leaflet from './leaflet/leaflet'; //also need the original version due to compass etc.
+import * as leaflet from './leaflet-rotate/leaflet-src';
 import * as map from './js/map'
 import './compass/leaflet-compass'; //if compile error from chokidar (C:\): Error: EBUSY: resource busy or locked --> adapt package.json, re-install npm etc.
 import './leaflet-routing-machine-3.2.12/dist/leaflet-routing-machine'; // routing machine for drawing route etc.
